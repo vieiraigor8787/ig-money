54a7b2f866fd66cd296be79ad67dcb73914b4fee
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-05-15 21:09:39 -0300

    config modal with react-model lib

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+import Modal from 'react-modal';
 import logo from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
 export function Header() {
+  const [isNewTransactionModal, setIsNewModalTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewModalTransactionModal(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewModalTransactionModal(false)
+  }
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="logo ig money" />
-        <button>Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>Nova transação</button>
+
+        <Modal isOpen={isNewTransactionModal} onRequestClose={handleCloseNewTransactionModal}>
+          <h2>cadastrar nova transação</h2>
+          </Modal>
       </Content>
     </Container>
   );
