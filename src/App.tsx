b5f1e74804787edff5dac2a57d424fc187df884e
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-05-15 21:19:24 -0300

    using props between comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
+
+import Modal from "react-modal";
+
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 import { Transactions } from "./components/TransactionsTable";
+
 import { GlobalStyle } from "./sytles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModal, setIsNewModalTransactionModal] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewModalTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewModalTransactionModal(false);
+  }
+
   return (
     <>
-      <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <Transactions />
+      <Modal
+        isOpen={isNewTransactionModal}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>cadastrar nova transação</h2>
+      </Modal>
+      <GlobalStyle />
     </>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,19 @@
-import { useState } from "react";
-import Modal from 'react-modal';
 import logo from "../../assets/logo.svg";
+
 import { Container, Content } from "./styles";
 
-export function Header() {
-  const [isNewTransactionModal, setIsNewModalTransactionModal] = useState(false);
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-  function handleOpenNewTransactionModal() {
-    setIsNewModalTransactionModal(true)
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewModalTransactionModal(false)
-  }
-
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logo} alt="logo ig money" />
-        <button type="button" onClick={handleOpenNewTransactionModal}>Nova transação</button>
-
-        <Modal isOpen={isNewTransactionModal} onRequestClose={handleCloseNewTransactionModal}>
-          <h2>cadastrar nova transação</h2>
-          </Modal>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
